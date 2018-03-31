2292bf0c5b1c954bc02eb631c25bdbef1ec3dd66
reklatsmasters <reklatsmasters@users.noreply.github.com>
2018-04-01 04:28:26 +0500

    simplified tests `streams/encode`.

--- test/stream/encode.js
+++ test/stream/encode.js
@@ -1,227 +1,96 @@
-const sinon = require('sinon')
+const Buffer = require('buffer').Buffer
 const EncodeStream = require('streams/encode')
 
+const BufferMock = {
+  alloc() {
+    return this
+  },
+
+  allocUnsafe() {
+    return this
+  },
+
+  writeDoubleBE: jest.fn(),
+  writeDoubleLE: jest.fn(),
+  writeFloatBE: jest.fn(),
+  writeFloatLE: jest.fn(),
+  writeInt8: jest.fn(),
+  writeInt16BE: jest.fn(),
+  writeInt16LE: jest.fn(),
+  writeInt32BE: jest.fn(),
+  writeInt32LE: jest.fn(),
+  writeIntBE: jest.fn(),
+  writeIntLE: jest.fn(),
+  writeUInt8: jest.fn(),
+  writeUInt16BE: jest.fn(),
+  writeUInt16LE: jest.fn(),
+  writeUInt32BE: jest.fn(),
+  writeUInt32LE: jest.fn(),
+  writeUIntBE: jest.fn(),
+  writeUIntLE: jest.fn(),
+}
+
+const methods = [
+  'writeDoubleBE',
+  'writeDoubleLE',
+  'writeFloatBE',
+  'writeFloatLE',
+  'writeInt8',
+  'writeInt16BE',
+  'writeInt16LE',
+  'writeInt32BE',
+  'writeInt32LE',
+  'writeIntBE',
+  'writeIntLE',
+  'writeUInt8',
+  'writeUInt16BE',
+  'writeUInt16LE',
+  'writeUInt32BE',
+  'writeUInt32LE',
+  'writeUIntBE',
+  'writeUIntLE',
+]
+
 describe('encode', () => {
-  const expectedNumber = 322
-  const mockBuffer = sinon.mock(Buffer.prototype)
+  const stream = new EncodeStream()
+  stream.append = jest.fn()
+
+  beforeAll(() => {
+    global.Buffer = BufferMock
+  })
+
+  afterAll(() => {
+    global.Buffer = Buffer
+  })
 
   afterEach(() => {
-    mockBuffer.restore()
+    stream.append.mockClear()
+
+    BufferMock.writeDoubleBE.mockClear()
+    BufferMock.writeDoubleLE.mockClear()
+    BufferMock.writeFloatBE.mockClear()
+    BufferMock.writeFloatLE.mockClear()
+    BufferMock.writeInt8.mockClear()
+    BufferMock.writeInt16BE.mockClear()
+    BufferMock.writeInt16LE.mockClear()
+    BufferMock.writeInt32BE.mockClear()
+    BufferMock.writeInt32LE.mockClear()
+    BufferMock.writeIntBE.mockClear()
+    BufferMock.writeIntLE.mockClear()
+    BufferMock.writeUInt8.mockClear()
+    BufferMock.writeUInt16BE.mockClear()
+    BufferMock.writeUInt16LE.mockClear()
+    BufferMock.writeUInt32BE.mockClear()
+    BufferMock.writeUInt32LE.mockClear()
+    BufferMock.writeUIntBE.mockClear()
+    BufferMock.writeUIntLE.mockClear()
   })
 
-  test('writeDoubleBE', () => {
-    mockBuffer
-      .expects('writeDoubleBE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeDoubleBE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeDoubleLE', () => {
-    mockBuffer
-      .expects('writeDoubleLE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeDoubleLE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeFloatBE', () => {
-    mockBuffer
-      .expects('writeFloatBE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeFloatBE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeFloatLE', () => {
-    mockBuffer
-      .expects('writeFloatLE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeFloatLE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeInt8', () => {
-    mockBuffer
-      .expects('writeInt8')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeInt8(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUInt8', () => {
-    mockBuffer
-      .expects('writeUInt8')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeUInt8(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeInt16BE', () => {
-    mockBuffer
-      .expects('writeInt16BE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeInt16BE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeInt16LE', () => {
-    mockBuffer
-      .expects('writeInt16LE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeInt16LE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeInt32BE', () => {
-    mockBuffer
-      .expects('writeInt32BE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeInt32BE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeInt32LE', () => {
-    mockBuffer
-      .expects('writeInt32LE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeInt32LE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUInt16BE', () => {
-    mockBuffer
-      .expects('writeUInt16BE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeUInt16BE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUInt16LE', () => {
-    mockBuffer
-      .expects('writeUInt16LE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeUInt16LE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUInt32BE', () => {
-    mockBuffer
-      .expects('writeUInt32BE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeUInt32BE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUInt32LE', () => {
-    mockBuffer
-      .expects('writeUInt32LE')
-      .once()
-      .withExactArgs(expectedNumber, 0)
-
-    const stream = new EncodeStream()
-    stream.writeUInt32LE(expectedNumber)
-
-    mockBuffer.verify()
-  })
-
-  test('writeIntBE', () => {
-    mockBuffer
-      .expects('writeIntBE')
-      .once()
-      .withExactArgs(expectedNumber, 0, 3)
-
-    const stream = new EncodeStream()
-    stream.writeIntBE(expectedNumber, 3)
-
-    mockBuffer.verify()
-  })
-
-  test('writeIntLE', () => {
-    mockBuffer
-      .expects('writeIntLE')
-      .once()
-      .withExactArgs(expectedNumber, 0, 3)
-
-    const stream = new EncodeStream()
-    stream.writeIntLE(expectedNumber, 3)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUIntBE', () => {
-    mockBuffer
-      .expects('writeUIntBE')
-      .once()
-      .withExactArgs(expectedNumber, 0, 3)
-
-    const stream = new EncodeStream()
-    stream.writeUIntBE(expectedNumber, 3)
-
-    mockBuffer.verify()
-  })
-
-  test('writeUIntLE', () => {
-    mockBuffer
-      .expects('writeUIntLE')
-      .once()
-      .withExactArgs(expectedNumber, 0, 3)
-
-    const stream = new EncodeStream()
-    stream.writeUIntLE(expectedNumber, 3)
-
-    mockBuffer.verify()
-  })
+  for (const method of methods) {
+    test(method, () => {
+      stream[method]()
+      expect(BufferMock[method]).toBeCalled()
+      expect(stream.append).toBeCalledWith(BufferMock)
+    })
+  }
 })
