49563a7c15cbe5305b2ff76b340afb45ef20d195
reklatsmasters <reklatsmasters@users.noreply.github.com>
2018-05-18 15:57:04 +0500

    null-string type now throws consistent errors.

---
+++ examples/decode-async.js
@@ -0,0 +1,27 @@
+const { Readable } = require('stream')
+const {
+  types: { string },
+  createDecodeStream,
+} = require('..')
+
+const NetworkPacket = string(null)
+
+const input = createDecodeStream(NetworkPacket)
+const source = new Readable({
+  read() {},
+})
+
+source.pipe(input).on('data', line => console.log(line))
+
+source.push('hello, ')
+process.nextTick(() => {
+  source.push('world!\0')
+})
+
+setTimeout(() => {
+  source.push('binary-')
+
+  setImmediate(() => {
+    source.push('data works!1\0')
+  })
+}, 0)
